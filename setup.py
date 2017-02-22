24b6b407407bb569f7b7e162254e741d82cf8986
sathishkumar <amsathishkumar@gmail.com>
2017-02-22 18:58:27 +0530

    package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/amsathishkumar/funniest',
+      author='Flying Circus',
+      author_email='amsathishkumar@gmail.com',
+      license='SAT',
+      packages=['funniest'],
+      zip_safe=True)
